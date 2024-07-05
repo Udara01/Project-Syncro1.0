ef9a6cf60006f65dc2fd7d4be7c6d7f8d893f304
UDARA NILUPUL <udaranilupul02@gmail.com>
2024-07-05 18:36:25 +0530

    Setting up basic environment for the Project

---
+++ react-project-syncro/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbarmain from './Components/Navbarmain';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarmain />
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ react-project-syncro/src/Components/Navbarmain.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Navbar, Nav, NavDropdown, Form, FormControl, InputGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaBell, FaSearch } from "react-icons/fa";
+import { IoSettingsOutline } from "react-icons/io5";
+import { AiOutlineUser } from "react-icons/ai";
+import { TiThMenu } from "react-icons/ti";
+
+import Sidebar from "./SidebarHome";
+
+function Navbarmain() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const handleSearch = () => {
+    alert('Search icon clicked');
+  };
+
+  return (
+    <Navbar bg="light" expand="lg" style={{ padding: '10px 20px' }}>
+      <Nav.Link onClick={toggleSidebar}><TiThMenu style={{ fontSize: '20px', marginRight: '20px' }} /></Nav.Link>
+      <Navbar.Brand href="#home">Syncro</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+
+        <Form className="mx-auto" style={{ width: '600px' }}>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for something"
+              className="mr-sm-2"
+              style={{ borderRadius: '20px', paddingRight: '40px' }}
+            />
+            <InputGroup.Text
+              onClick={handleSearch}
+              style={{
+                position: 'absolute',
+                right: '10px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                background: 'transparent',
+                border: 'none',
+                cursor: 'pointer'
+              }}
+            >
+              <FaSearch />
+            </InputGroup.Text>
+          </InputGroup>
+        </Form>
+        <Nav className="align-items-center" style={{ marginRight: '20px' }}>
+
+          <Nav.Link href="#notifications"><FaBell style={{ fontSize: '20px', marginRight: '20px' }} /></Nav.Link>
+
+          <NavDropdown
+            title={<AiOutlineUser style={{ fontSize: '24px', }} />}
+            id="basic-nav-dropdown"
+            alignRight
+          >
+            <NavDropdown.Item href="#profile">John John</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#logout">Log out</NavDropdown.Item>
+          </NavDropdown>
+
+          <Nav.Link href="#settings"><IoSettingsOutline style={{ fontSize: '22px', marginRight: '40px', marginLeft: '20px' }} /></Nav.Link>
+
+        </Nav>
+      </Navbar.Collapse>
+
+      {/* Render Sidebar conditionally based on showSidebar state */}
+      {showSidebar && <Sidebar />}
+    </Navbar>
+  );
+}
+
+export default Navbarmain;
